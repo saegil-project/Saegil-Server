0bf818769f8f5afe221c76235cf345fac995f7ef
kwonyj1022 <kwonyj1022@naver.com>
2025-04-06 20:22:24 +0900

    docs: 공지사항 목록 조회 응답에 웹링크 추가

--- src/main/java/com/newworld/saegil/notice/controller/ReadNoticesResponse.java
+++ src/main/java/com/newworld/saegil/notice/controller/ReadNoticesResponse.java
@@ -20,7 +20,7 @@ public record ReadNoticesResponse(
             @Schema(description = "공지사항 식별자", example = "1")
             Long id,
 
-            @Schema(description = "공지사항 제목", example = "공지사항 제목")
+            @Schema(description = "공지사항 제목", example = "2025년도 경남지역적응센터(하나센터) 채용 공고(~4.15)")
             String title,
 
             @Schema(description = "공지사항 내용", example = "공지사항 내용")
@@ -29,8 +29,12 @@ public record ReadNoticesResponse(
             @Schema(description = "기관", example = "기관 이름")
             String source,
 
+            @Schema(description = "공지사항 등록일", example = "2025.04.01.")
             @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd.")
-            LocalDate date
+            LocalDate date,
+
+            @Schema(description = "공지사항 URL", example = "https://www.koreahana.or.kr/home/kor/board.do?&menuPos=54&act=detail&idx=19193&boardPwd=&searchValue1=0&searchKeyword=&pageIndex=1")
+            String webLink
     ) {
     }
 }
