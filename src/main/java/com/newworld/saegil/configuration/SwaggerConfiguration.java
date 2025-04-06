d996d81f39810c1f9263676d6c951cd24ba8def7
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-04-06 14:10:23 +0900

    feat: Swagger 설정 추가 및 api 명세 작성 (#6)
    * chore: swagger 의존성 추가
    
    * feat: Swagger 설정 추가
    
    * docs: 유저 API 명세 추가
    
    * docs: 인증 API 명세 추가
    
    * docs: 공지사항 API 명세 추가
    
    * docs: 기관 API 명세 추가
    
    * docs: 유저, 공지사항, 기관 API에 더미데이터 추가
    
    * refactor: Swagger Security Scheme 설정 부분 리팩토링

---
+++ src/main/java/com/newworld/saegil/configuration/SwaggerConfiguration.java
@@ -0,0 +1,56 @@
+package com.newworld.saegil.configuration;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    public static final String SERVICE_SECURITY_SCHEME_NAME = "ServiceAuth";
+    public static final String OAUTH_SECURITY_SCHEME_NAME = "KakaoAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        final Info apiInfo = new Info().title("Saegil API Document").version("v0.0.1");
+        return new OpenAPI()
+                .info(apiInfo)
+                .components(components())
+                .addSecurityItem(new SecurityRequirement().addList(SERVICE_SECURITY_SCHEME_NAME))
+                .addSecurityItem(new SecurityRequirement().addList(OAUTH_SECURITY_SCHEME_NAME));
+    }
+
+    private Components components() {
+        return new Components()
+                .addSecuritySchemes(SERVICE_SECURITY_SCHEME_NAME, serviceSecurityScheme())
+                .addSecuritySchemes(OAUTH_SECURITY_SCHEME_NAME,oauthSecurityScheme());
+    }
+
+    private SecurityScheme serviceSecurityScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("서비스에서 발급한 Bearer Token");
+    }
+
+    private SecurityScheme oauthSecurityScheme() {
+        final OAuthFlow authorizationCodeFlow = new OAuthFlow()
+                .authorizationUrl("https://kauth.kakao.com/oauth/authorize")
+                .tokenUrl("https://kauth.kakao.com/oauth/token");
+
+        final OAuthFlows oauthFlows = new OAuthFlows().authorizationCode(authorizationCodeFlow);
+
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.OAUTH2)
+                .flows(oauthFlows)
+                .description("Kakao OAuth 인증");
+    }
+}
