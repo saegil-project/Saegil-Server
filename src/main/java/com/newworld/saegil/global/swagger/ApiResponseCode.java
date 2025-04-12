ca51ba51d8b2163077bc3062cc62e20a6d64e183
kwonyj1022 <kwonyj1022@naver.com>
2025-04-12 15:58:14 +0900

    feat: Swagger `@ApiResponse`의 responseCode에 사용할 상수 정의 클래스 추가

---
+++ src/main/java/com/newworld/saegil/global/swagger/ApiResponseCode.java
@@ -0,0 +1,16 @@
+package com.newworld.saegil.global.swagger;
+
+public class ApiResponseCode {
+    public static final String OK = "200";
+    public static final String CREATED = "201";
+    public static final String NO_CONTENT = "204";
+    public static final String FOUND = "302";
+    public static final String BAD_REQUEST = "400";
+    public static final String UNAUTHORIZED = "401";
+    public static final String FORBIDDEN = "403";
+    public static final String NOT_FOUND = "404";
+    public static final String INTERNAL_SERVER_ERROR = "500";
+
+    private ApiResponseCode() {
+    }
+}
