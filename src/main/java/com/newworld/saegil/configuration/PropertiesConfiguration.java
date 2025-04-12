36da087666e120952ddab78d925f2a6d5f26a64d
kwonyj1022 <kwonyj1022@naver.com>
2025-04-12 21:51:30 +0900

    feat: LLM 프록시 서버 url 설정 추가

--- src/main/java/com/newworld/saegil/configuration/PropertiesConfiguration.java
+++ src/main/java/com/newworld/saegil/configuration/PropertiesConfiguration.java
@@ -1,11 +1,12 @@
 package com.newworld.saegil.configuration;
 
+import com.newworld.saegil.proxy.config.ProxyProperties;
 import com.newworld.saegil.security.jwt.JwtProperties;
 import com.newworld.saegil.security.oauth2.KakaoOAuth2Properties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({JwtProperties.class, KakaoOAuth2Properties.class})
+@EnableConfigurationProperties({JwtProperties.class, KakaoOAuth2Properties.class, ProxyProperties.class})
 public class PropertiesConfiguration {
 }
---
+++ src/main/java/com/newworld/saegil/proxy/config/ProxyProperties.java
@@ -0,0 +1,44 @@
+package com.newworld.saegil.proxy.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "proxy.llm")
+public record ProxyProperties(
+        String llmServerUrl,
+        String ttsPath,
+        String sttPath,
+        String sttFromAudioFilePath,
+        String chatgptFromTextPath,
+        String chatgptFromSttTextPath,
+        String chatgptFromAudioUrlPath,
+        String chatgptFromAudioFilePath
+) {
+
+    public String getTtsPath() {
+        return llmServerUrl + ttsPath;
+    }
+
+    public String getSttPath() {
+        return llmServerUrl + sttPath;
+    }
+
+    public String getSttFromAudioFilePath() {
+        return llmServerUrl + sttFromAudioFilePath;
+    }
+
+    public String getChatgptFromTextPath() {
+        return llmServerUrl + chatgptFromTextPath;
+    }
+
+    public String getChatgptFromSttTextPath() {
+        return llmServerUrl + chatgptFromSttTextPath;
+    }
+
+    public String getChatgptFromAudioUrlPath() {
+        return llmServerUrl + chatgptFromAudioUrlPath;
+    }
+
+    public String getChatgptFromAudioFilePath() {
+        return llmServerUrl + chatgptFromAudioFilePath;
+    }
+}
