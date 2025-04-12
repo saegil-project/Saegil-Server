41ded03441f9f60812918e461dd7e1315a1ce12a
kwonyj1022 <kwonyj1022@naver.com>
2025-04-13 00:39:55 +0900

    feat: 인증 관련 Interceptor와 ArgumentResolver 설정 추가

---
+++ src/main/java/com/newworld/saegil/configuration/WebConfiguration.java
@@ -0,0 +1,36 @@
+package com.newworld.saegil.configuration;
+
+import com.newworld.saegil.authentication.interceptor.LoginInterceptor;
+import com.newworld.saegil.authentication.resolver.AuthUserInfoArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final LoginInterceptor loginInterceptor;
+    private final AuthUserInfoArgumentResolver authUserInfoArgumentResolver;
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/")
+                .excludePathPatterns("/index.html")
+                .excludePathPatterns("/test-llm-proxy.html")
+                .excludePathPatterns("/api/v1/organizations/**")
+                .excludePathPatterns("/api/v1/notices/**")
+                .excludePathPatterns("/api/v1/oauth2/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authUserInfoArgumentResolver);
+    }
+}
