a92b9454a52d2a10eefc820304d7694b60395c60
kwonyj1022 <kwonyj1022@naver.com>
2025-04-26 00:20:23 +0900

    refactor: 함수 분리 제거로 가독성 향상

--- src/main/java/com/newworld/saegil/authentication/service/AuthenticationService.java
+++ src/main/java/com/newworld/saegil/authentication/service/AuthenticationService.java
@@ -40,19 +40,14 @@ public class AuthenticationService {
             final String authorizationCode,
             final LocalDateTime requestTime
     ) {
-        final OAuth2UserInfo oauth2UserInfo = fetchOAuth2UserInfo(oauth2TypeName, authorizationCode);
+        final OAuth2Handler oauth2Handler = oauth2HandlerComposite.findHandler(oauth2TypeName);
+        final String oauth2AccessToken = oauth2Handler.getOAuth2AccessToken(authorizationCode);
+        final OAuth2UserInfo oauth2UserInfo = oauth2Handler.getUserInfo(oauth2AccessToken);
         final User user = findOrPersistUser(oauth2UserInfo);
         final PrivateClaims privateClaims = new PrivateClaims(user.getId());
         final Token token = tokenProcessor.generateToken(requestTime, privateClaims.toMap());
 
         return new LoginResult(user.getId(), token.accessToken(), token.refreshToken());
-    }
-
-    private OAuth2UserInfo fetchOAuth2UserInfo(final String oauth2TypeName, final String authorizationCode) {
-        final OAuth2Handler oauth2Handler = oauth2HandlerComposite.findHandler(oauth2TypeName);
-        final String oauth2AccessToken = oauth2Handler.getOAuth2AccessToken(authorizationCode);
-
-        return oauth2Handler.getUserInfo(oauth2AccessToken);
     }
 
     private User findOrPersistUser(final OAuth2UserInfo oauth2UserInfo) {
