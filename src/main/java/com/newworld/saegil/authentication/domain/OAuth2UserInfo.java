acd5999a7d9e79afa288a3ac944c024b2f3be526
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-05-01 00:49:22 +0900

    feat: 유저 생성 시 프로필 이미지에 카카오 프로필 이미지 대신 기본 이미지 사용 (#37)
    * feat: 유저 생성 시 프로필이미지 url을 기본 이미지 url로 설정
    
    * refactor: oauth 사용자 정보를 가져올 때 프로필 이미지 제외

--- src/main/java/com/newworld/saegil/authentication/domain/OAuth2UserInfo.java
+++ src/main/java/com/newworld/saegil/authentication/domain/OAuth2UserInfo.java
@@ -3,7 +3,6 @@ package com.newworld.saegil.authentication.domain;
 public record OAuth2UserInfo(
         String oauth2Id,
         OAuth2Type oauth2Type,
-        String nickname,
-        String profileImageUrl
+        String nickname
 ) {
 }
