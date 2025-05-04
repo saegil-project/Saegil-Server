ab744c5f064ab836c76054d989f79e11faa33b50
kwonyj1022 <kwonyj1022@naver.com>
2025-05-05 08:11:51 +0900

    feat: 시뮬레이션 대화 내용 조회 api 추가

---
+++ src/main/java/com/newworld/saegil/simulation/controller/ReadSimulationMessagesResponse.java
@@ -0,0 +1,38 @@
+package com.newworld.saegil.simulation.controller;
+
+import com.newworld.saegil.simulation.service.MessageDto;
+import com.newworld.saegil.simulation.service.ReadMessagesResult;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ReadSimulationMessagesResponse(
+        String scenarioName,
+        List<ReadMessageItemResponse> messages
+) {
+
+    public static ReadSimulationMessagesResponse from(final ReadMessagesResult readMessagesResult) {
+        final String scenarioName = readMessagesResult.simulation().scenario().name();
+        final List<ReadMessageItemResponse> messages = readMessagesResult.messages().stream()
+                                                                      .map(ReadMessageItemResponse::from)
+                                                                      .toList();
+
+        return new ReadSimulationMessagesResponse(scenarioName, messages);
+    }
+
+    record ReadMessageItemResponse(
+            Long id,
+            boolean isFromUser,
+            String contents,
+            LocalDateTime createdAt
+    ) {
+        public static ReadMessageItemResponse from(final MessageDto messageDto) {
+            return new ReadMessageItemResponse(
+                    messageDto.id(),
+                    messageDto.isFromUser(),
+                    messageDto.contents(),
+                    messageDto.createdAt()
+            );
+        }
+    }
+}
--- src/main/java/com/newworld/saegil/simulation/controller/SimulationController.java
+++ src/main/java/com/newworld/saegil/simulation/controller/SimulationController.java
@@ -4,15 +4,18 @@ import com.newworld.saegil.authentication.annotation.AuthUser;
 import com.newworld.saegil.authentication.dto.AuthUserInfo;
 import com.newworld.saegil.configuration.SwaggerConfiguration;
 import com.newworld.saegil.global.swagger.ApiResponseCode;
+import com.newworld.saegil.simulation.service.ReadMessagesResult;
 import com.newworld.saegil.simulation.service.SimulationDto;
 import com.newworld.saegil.simulation.service.SimulationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +41,23 @@ public class SimulationController {
         final List<ReadSimulationResponse> response = simulationDtos.stream()
                                                                     .map(ReadSimulationResponse::from)
                                                                     .toList();
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/{simulationId}/messages")
+    @Operation(
+            summary = "시뮬레이션 대화 메시지 내용 목록 조회",
+            description = "하나의 시뮬레이션 대화 메시지 내용 목록을 조회합니다.",
+            security = @SecurityRequirement(name = SwaggerConfiguration.SERVICE_SECURITY_SCHEME_NAME)
+    )
+    @ApiResponse(responseCode = ApiResponseCode.OK, description = "시뮬레이션 대화 메시지 내용 목록 조회 성공")
+    public ResponseEntity<ReadSimulationMessagesResponse> readAllMessagesBySimulationId(
+            @Parameter(description = "시뮬레이션 기록 식별자", example = "1")
+            @PathVariable final Long simulationId
+    ) {
+        final ReadMessagesResult result = simulationService.readAllMessagesBySimulationId(simulationId);
+        final ReadSimulationMessagesResponse response = ReadSimulationMessagesResponse.from(result);
 
         return ResponseEntity.ok(response);
     }
