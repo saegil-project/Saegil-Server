5f8f9dd02ba77b4d41b7543f639726e9e7ef39e1
kwonyj1022 <kwonyj1022@naver.com>
2025-05-06 02:01:49 +0900

    fix: 시뮬레이션 entity에 누락된 id 생성 전략 추가

--- src/main/java/com/newworld/saegil/simulation/domain/Simulation.java
+++ src/main/java/com/newworld/saegil/simulation/domain/Simulation.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -27,7 +28,7 @@ import java.time.LocalDateTime;
 public class Simulation {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.EAGER)
