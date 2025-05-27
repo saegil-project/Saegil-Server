b42fa4103cbabc4df71e1cb2528ed15c4c25e6dd
kwonyj1022 <kwonyj1022@naver.com>
2025-05-27 20:01:30 +0900

    refactor: 주변 시설 조회 시 bounding box 사용해서 조회하도록 개선

--- src/main/java/com/newworld/saegil/facility/repository/FacilityRepository.java
+++ src/main/java/com/newworld/saegil/facility/repository/FacilityRepository.java
@@ -2,6 +2,7 @@ package com.newworld.saegil.facility.repository;
 
 import com.newworld.saegil.facility.domain.Facility;
 import com.newworld.saegil.facility.domain.FacilityInfoSource;
+import com.newworld.saegil.location.GeoBoundingBox;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -39,4 +40,14 @@ public interface FacilityRepository extends JpaRepository<Facility, Long> {
             @Param("longitude") double longitude,
             @Param("radius") double radius
     );
+
+
+    @Query("""
+            SELECT f
+            FROM Facility f
+            WHERE f.latitude IS NOT NULL AND f.longitude IS NOT NULL
+              AND f.latitude BETWEEN :#{#geoBoundingBox.minLatitude} AND :#{#geoBoundingBox.maxLatitude}
+              AND f.longitude BETWEEN :#{#geoBoundingBox.minLongitude} AND :#{#geoBoundingBox.maxLongitude}
+            """)
+    List<Facility> findAllInBoundingBox(GeoBoundingBox geoBoundingBox);
 }
--- src/main/java/com/newworld/saegil/facility/service/FacilityService.java
+++ src/main/java/com/newworld/saegil/facility/service/FacilityService.java
@@ -1,8 +1,11 @@
 package com.newworld.saegil.facility.service;
 
+import com.newworld.saegil.facility.domain.Facility;
 import com.newworld.saegil.facility.repository.FacilityAndDistance;
 import com.newworld.saegil.facility.repository.FacilityRepository;
+import com.newworld.saegil.location.GeoBoundingBox;
 import com.newworld.saegil.location.GeoPoint;
+import com.newworld.saegil.location.GeoUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,14 +23,24 @@ public class FacilityService {
             final GeoPoint baseGeoPoint,
             final int radius
     ) {
-        final List<FacilityAndDistance> nearbyFacilities = facilityRepository.findNearbyFacilities(
+        final GeoBoundingBox geoBoundingBox = GeoUtils.calculateBoundingBox(
                 baseGeoPoint.latitude(),
                 baseGeoPoint.longitude(),
                 radius
         );
+        final List<Facility> facilitiesInBoundingBox = facilityRepository.findAllInBoundingBox(geoBoundingBox);
 
-        return nearbyFacilities.stream()
+        return facilitiesInBoundingBox.stream()
+                               .map(facility -> mapWithDistance(facility, baseGeoPoint))
+                               .filter(facilityAndDistance -> facilityAndDistance.distance() <= radius)
                                .map(FacilityAndDistance::toNearbyFacilityDto)
                                .toList();
+    }
+
+    private FacilityAndDistance mapWithDistance(final Facility facility, final GeoPoint baseGeoPoint) {
+        return new FacilityAndDistance(
+                facility,
+                GeoUtils.calculateDistanceMeters(baseGeoPoint, facility.getGeoPoint())
+        );
     }
 }
