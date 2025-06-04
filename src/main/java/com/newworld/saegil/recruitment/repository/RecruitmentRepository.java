4a70d3ce069070979fb4e3a356b61e04a2f89a5a
kwonyj1022 <kwonyj1022@naver.com>
2025-06-04 17:15:30 +0900

    feat: bounding box 내부의 채용 정보 조회 메서드 추가

--- src/main/java/com/newworld/saegil/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/newworld/saegil/recruitment/repository/RecruitmentRepository.java
@@ -1,7 +1,20 @@
 package com.newworld.saegil.recruitment.repository;
 
+import com.newworld.saegil.location.GeoBoundingBox;
 import com.newworld.saegil.recruitment.domain.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+
+    @Query("""
+            SELECT r
+            FROM Recruitment r
+            WHERE r.latitude IS NOT NULL AND r.longitude IS NOT NULL
+              AND r.latitude BETWEEN :#{#geoBoundingBox.minLatitude} AND :#{#geoBoundingBox.maxLatitude}
+              AND r.longitude BETWEEN :#{#geoBoundingBox.minLongitude} AND :#{#geoBoundingBox.maxLongitude}
+            """)
+    List<Recruitment> findAllInBoundingBox(final GeoBoundingBox geoBoundingBox);
 }
