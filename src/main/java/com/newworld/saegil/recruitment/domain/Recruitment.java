5634b610835fe92219f53cbee7fe45dc9454318f
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2025-06-04 16:33:06 +0900

    feat: 서울열린데이터광장 서울시일자리포털 채용 정보 및 근무지 위/경도 저장 기능 추가 (#62)
    * feat: 설정 파일에 서울열린데이터광장 인증키 추가
    
    * feat: 채용 정보 entity 및 repository 추가
    
    * feat: 서울일자리포털 채용 정보 크롤링 기능 추가
    
    * feat: 채용정보 크롤링 및 근무지 위/경도 좌표 저장

---
+++ src/main/java/com/newworld/saegil/recruitment/domain/Recruitment.java
@@ -0,0 +1,105 @@
+package com.newworld.saegil.recruitment.domain;
+
+import com.newworld.saegil.location.LocationInfo;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@EqualsAndHashCode(of = "id", callSuper = false)
+@ToString
+@Table(name = "recruitment")
+public class Recruitment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String recruitmentCode;
+
+    @Column(nullable = false)
+    private RecruitmentInfoSource infoSource;
+
+    @Column(nullable = false)
+    private String name;
+
+    private LocalDateTime recruitmentStartDate;
+
+    private LocalDateTime recruitmentEndDate;
+
+    private String weeklyWorkdays;
+
+    private String workTime;
+
+    private String pay;
+
+    private String webLink;
+
+    private String roadAddress;
+
+    private String jibunAddress;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private String errorMessage;
+
+    public Recruitment(
+            final String recruitmentCode,
+            final RecruitmentInfoSource infoSource,
+            final String name,
+            final LocalDateTime recruitmentStartDate,
+            final LocalDateTime recruitmentEndDate,
+            final String weeklyWorkdays,
+            final String workTime,
+            final String pay,
+            final String webLink,
+            final String roadAddress
+    ) {
+        this.recruitmentCode = recruitmentCode;
+        this.infoSource = infoSource;
+        this.name = name.trim();
+        this.recruitmentStartDate = recruitmentStartDate;
+        this.recruitmentEndDate = recruitmentEndDate;
+        this.weeklyWorkdays = weeklyWorkdays.trim();
+        this.workTime = workTime.trim();
+        this.pay = pay.trim();
+        this.webLink = webLink;
+        this.roadAddress = roadAddress.trim();
+    }
+
+    public void updateLocationInfo(final LocationInfo locationInfo) {
+        this.roadAddress = locationInfo.roadAddress();
+        this.jibunAddress = locationInfo.jibunAddress();
+        this.latitude = locationInfo.getLatitude();
+        this.longitude = locationInfo.getLongitude();
+    }
+
+
+    public void markError(final Exception exception) {
+        if (exception == null) {
+            return;
+        }
+
+        final String exceptionMeesage = exception.getClass().getSimpleName() + ": " + exception.getMessage();
+
+        if (exceptionMeesage.length() > 250) {
+            this.errorMessage = exceptionMeesage.substring(0, 250) + "...";
+        } else {
+            this.errorMessage = exceptionMeesage;
+        }
+    }
+}
