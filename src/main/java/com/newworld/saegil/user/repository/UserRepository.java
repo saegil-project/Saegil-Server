dde668a5c901e6e4b4f6bc69481f22bd60863f9a
kwonyj1022 <kwonyj1022@naver.com>
2025-07-13 22:59:11 +0900

    feat: 새로운 공지사항 감지 시 사용자에게 알림 전송

--- src/main/java/com/newworld/saegil/user/repository/UserRepository.java
+++ src/main/java/com/newworld/saegil/user/repository/UserRepository.java
@@ -3,12 +3,17 @@ package com.newworld.saegil.user.repository;
 import com.newworld.saegil.authentication.domain.OAuth2Type;
 import com.newworld.saegil.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByOauth2IdAndOauth2Type(String oauth2Id, OAuth2Type oauth2Type);
+
+    @Query("SELECT u.id FROM User u WHERE u.deviceToken IS NOT NULL")
+    List<Long> findAllUserIdsWithDeviceToken();
 }
